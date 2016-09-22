b2544dba585fc7a027fe13891b4f499e047ade78
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2016-09-22 13:36:10 +0200

    expose notify function

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ module.exports = function(sails) {
     
     var say = require('./lib/say.js');
     var install = require('./lib/install.js');
+    var notify = require('./lib/notify.js');
 
     return {
         say: say,
-        install: install
+        install: install,
+        notify: notify
     };
 };
